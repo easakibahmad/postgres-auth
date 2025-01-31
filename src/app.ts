a23715a448ff76874aa8b0ca30e30ac3dd0eb815
sakib.ahmad <sakib.ahmad@yaana.com>
2025-01-31 18:26:28 +0600

    add cors

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,18 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/userRoute'; 
 import { logger } from './middleware/logger';
 import { errorHandler } from './middleware/errorHandler';
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 
 app.use(logger);
 
